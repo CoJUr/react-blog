af7258d983c2280ae79430fb7f168559e12f2a94
“Cory <coryurton@gmail.com>
2022-08-20 12:54:23 -0500

    intro to states and reactive hooks

--- src/Home.js
+++ src/Home.js
@@ -1,17 +1,21 @@
+import { useState } from 'react';
+
 const Home = () => {
+    // let name = 'luigi';
+    const [name, setName] = useState('mario');
+    const [age, setAge] = useState(25);
 
-    const handleClick = (e) => {
-        console.log('Thanks for clicking', e)
+    const handleClick = () => {
+        setName('luigi');
+        setAge(30);
     }
-
-    const handleAnotherClick = (name, e) => {console.log('hello  ' + name, e.target)}
 
 
     return (
         <div className="home">
         <h2>Homepage</h2>
+            <p> { name } is { age } years old </p>
             <button onClick={handleClick}>Click here</button>
-            <button onClick={(e) => handleAnotherClick('Jake', e)}>Click me again</button>
         </div>
     );
 }

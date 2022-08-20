b93dc803ecdc0991fab5ac065ac91f5a71f2d6bf
“Cory <coryurton@gmail.com>
2022-08-20 12:38:02 -0500

    buttons galore

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,17 @@
 const Home = () => {
+
+    const handleClick = (e) => {
+        console.log('Thanks for clicking', e)
+    }
+
+    const handleAnotherClick = (name, e) => {console.log('hello  ' + name, e.target)}
+
+
     return (
         <div className="home">
         <h2>Homepage</h2>
+            <button onClick={handleClick}>Click here</button>
+            <button onClick={(e) => handleAnotherClick('Jake', e)}>Click me again</button>
         </div>
     );
 }
